7a8898f261e2919f6fe334dd9b8495b0ba2e3cdb
iamamit-107 <ahmed.amit107@gmail.com>
2021-09-15 19:50:09 +0600

    add a component with object type props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Greeting from "./components/Greeting";
+import { Person } from "./components/Person";
 
 function App() {
+  const person = {
+    first: "ahmed",
+    last: "faisal",
+  };
   return (
     <div className="App">
       <Greeting name="Amit" place="React-Typescript" />
+      <Person name={person} />
     </div>
   );
 }
