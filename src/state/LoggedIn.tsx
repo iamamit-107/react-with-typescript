4d013523e306202dadbf7d8dda4813e715094bfc
iamamit-107 <ahmed.amit107@gmail.com>
2021-09-21 12:13:24 +0600

    useState for future value

--- src/state/LoggedIn.tsx
+++ src/state/LoggedIn.tsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 
+type AuthUser = {
+  name: string;
+  email: string;
+};
+
 export default function LoggedIn() {
-  const [loggedin, setLoggedin] = useState(false);
+  const [loggedin, setLoggedin] = useState<null | AuthUser>(null);
 
   const handleLogin = () => {
-    setLoggedin(true);
+    setLoggedin({
+      name: "Amit",
+      email: "amit@amit.com",
+    });
   };
   const handleLogout = () => {
-    setLoggedin(false);
+    setLoggedin(null);
   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
 
-      <h3>User is {loggedin ? `logged in` : `Logged out`}</h3>
+      <h3>Name is {loggedin?.name}</h3>
+      <h3>Email is {loggedin?.email}</h3>
     </div>
   );
 }
